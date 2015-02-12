80666d55de7daa97aeeae0ce36cb7232a29a4158
pdebelak <pdebelak@gmail.com>
2015-02-12 15:52:41 -0600

    Change parser to just operate on a single line

--- person_file_parser/lib/person_file_parser/parser.rb
+++ person_file_parser/lib/person_file_parser/parser.rb
@@ -1,43 +1,24 @@
 module PersonFileParser
   class Parser
 
-    def initialize(file)
-      @file = file
-    end
-
-    def parse
-      parsed_file = []
-      File.open(file, "r") do |f|
-        f.each_line do |line|
-          parsed_file << separated(line.chomp)
-        end
-      end
-      parsed_file
+    def parse(entry)
+      split(entry).each_with_index.map do |item, index|
+        [KEYS[index], item]
+      end.to_h
     end
 
     private
 
-      attr_reader :file
-
       KEYS = [:last_name, :first_name, :gender, :favorite_color, :date_of_birth]
 
-      def separated(line)
-        separated = {}
-        line.split(separator).each_with_index do |item, index|
-          separated[KEYS[index]] = item
-        end
-        separated
+      def split(entry)
+        entry.split(separator entry)
       end
 
-      def separator
-        @separator ||= determine_separator
-      end
-
-      def determine_separator
-        first_line = File.open(file, &:readline)
-        if first_line =~ / \| /
+      def separator(entry)
+        if entry =~ / \| /
           " | "
-        elsif first_line =~ /, /
+        elsif entry =~ /, /
           ", "
         end
       end
--- person_file_parser/spec/person_file_parser/parser_spec.rb
+++ person_file_parser/spec/person_file_parser/parser_spec.rb
@@ -3,56 +3,43 @@ require_relative '../spec_helper'
 module PersonFileParser
   describe Parser do
 
-    describe '#initialize' do
-
-      it 'takes one argument' do
-        expect(Parser.new 'a file').to be_an_instance_of PersonFileParser::Parser
-      end
-
-      it 'raises an error with no arguments' do
-        expect{ Parser.new }.to raise_error ArgumentError
-      end
-    end
-
     describe '#parse' do
 
-      context 'with a pipe separated file' do
-        let(:parser) { Parser.new 'spec/fixtures/files/pipe_separated_file' }
+      let(:parser) { Parser.new }
 
-        it 'returns an array' do
-          expect(parser.parse).to be_an_instance_of Array
-        end
+      context 'with a pipe separated string' do
 
-        it 'creates a hash for each line' do
-          expect(parser.parse.first).to eq({ last_name: 'Debelak', 
-                                             first_name: 'Peter', 
-                                             gender: 'male', 
-                                             favorite_color: 'orange', 
-                                             date_of_birth: '03/20/1985' })
+        it 'returns an organized hash' do
+          expect(parser.parse 'Debelak | Peter | male | orange | 03/20/1985')
+            .to eq({ last_name: 'Debelak', 
+                     first_name: 'Peter', 
+                     gender: 'male', 
+                     favorite_color: 'orange', 
+                     date_of_birth: '03/20/1985' })
         end
       end
 
-      context 'with a comma separated file' do
-        let(:parser) { Parser.new 'spec/fixtures/files/comma_separated_file' }
+      context 'with a comma separated string' do
 
-        it 'creates a hash for each line' do
-          expect(parser.parse.first).to eq({ last_name: 'Debelak', 
-                                             first_name: 'Peter', 
-                                             gender: 'male', 
-                                             favorite_color: 'orange', 
-                                             date_of_birth: '03/20/1985' })
+        it 'returns an organized hash' do
+          expect(parser.parse 'Debelak, Peter, male, orange, 03/20/1985')
+            .to eq({ last_name: 'Debelak', 
+                     first_name: 'Peter', 
+                     gender: 'male', 
+                     favorite_color: 'orange', 
+                     date_of_birth: '03/20/1985' })
         end
       end
 
-      context 'with a space separated file' do
-        let(:parser) { Parser.new 'spec/fixtures/files/space_separated_file' }
+      context 'with a space separated string' do
 
-        it 'creates a hash for each line' do
-          expect(parser.parse.first).to eq({ last_name: 'Debelak', 
-                                             first_name: 'Peter', 
-                                             gender: 'male', 
-                                             favorite_color: 'orange', 
-                                             date_of_birth: '03/20/1985' })
+        it 'returns an organized hash' do
+          expect(parser.parse 'Debelak Peter male orange 03/20/1985')
+            .to eq({ last_name: 'Debelak', 
+                     first_name: 'Peter', 
+                     gender: 'male', 
+                     favorite_color: 'orange', 
+                     date_of_birth: '03/20/1985' })
         end
       end
     end
